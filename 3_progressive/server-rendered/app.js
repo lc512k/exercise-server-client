59eae16e1d1275a3cc5b50a5b332c2b386370fe9
Laura Carvajal <laura.carvajal@gmail.com>
2017-02-21 22:10:10 +0000

    enhanced

---
+++ 3_progressive/server-rendered/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+// Set up handlebars
+app.engine('html', exphbs({
+		defaultLayout: 'main',
+		extname: '.html',
+		layoutsDir: 'views/layouts/'
+	})
+);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+
+	const colorNames = ['red', 'orange', 'green'];
+
+	const randomIndex = Math.floor(Math.random() * 3);
+
+	res.render('home', {
+		randomColor: colorNames[randomIndex]
+	});
+})
+
+app.listen(process.env.PORT || 8001, () => {
+	console.log('3_progressive app: listening on port 8001');
+});
