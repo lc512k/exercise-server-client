6fc8de93bb12787586633aedf6626264a56048b4
Laura Carvajal <laura.carvajal@gmail.com>
2017-02-21 18:32:39 +0000

    wip

--- starter/server-rendered/app.js
+++ starter/server-rendered/app.js
@@ -1,8 +1,11 @@
+
 const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-	res.render('<html><body>Hello world (server)</body></html>')
+	res.send('<html><body>Hello world (server)</body></html>')
 })
 
-app.listen(process.env.PORT || 8001, () => {});
+app.listen(process.env.PORT || 8001, () => {
+	console.log('listening on port 8001');
+});
